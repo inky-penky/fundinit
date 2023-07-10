4ff2406cfac8744660b188a612515948b0b6c8fb
Mustapha Shuaibu <119011957+Mplanner12@users.noreply.github.com>
2023-07-10 08:36:07 +0100

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,10 +3,10 @@ import Link from "next/link"
 const Navbar = () => {
   return (
     <div className="flex justify-end pt-8 pr-[10rem] text-white bg-transparent gap-8">
-        <Link href="#" alt="home" legacyBehavior><a className="hover:text-purple-700">Home</a></Link>
-        <Link href="#" alt="home" legacyBehavior><a className="hover:text-purple-700">Companies</a></Link>
-        <Link href="#" alt="home" legacyBehavior><a className="hover:text-purple-700">Fellowship</a></Link>
-        <Link href="#" alt="home" legacyBehavior><a className="hover:text-purple-700">Team</a></Link>
+        <Link href="#" alt="Home" legacyBehavior><a className="hover:text-purple-700">Home</a></Link>
+        <Link href="#" alt="Companies" legacyBehavior><a className="hover:text-purple-700">Companies</a></Link>
+        <Link href="#" alt="Fellowship" legacyBehavior><a className="hover:text-purple-700">Fellowship</a></Link>
+        <Link href="#" alt="Team" legacyBehavior><a className="hover:text-purple-700">Team</a></Link>
     </div>
   )
 }
