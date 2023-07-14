ca2593ff912dd0d1f0000d7ba181bc7ae45717d5
ahmad abdulazeez <ahmadabdulazeez354@gmail.com>
2023-07-14 08:52:11 +0100

    update: profile links

--- components/Footer.js
+++ components/Footer.js
@@ -8,16 +8,16 @@ import Email from '@/public/images/mail.png'
 const Footer = () => {
   return (
     <div className="flex items-center justify-center w-full h-auto gap-8 py-3 pb-6 bg-white">
-        <a href="" className="w-[1.4rem]" alt="linkedin">
+        <a href="https://www.linkedin.com/company/fundinit/" className="w-[1.4rem]" alt="linkedin">
             <Image src={LinkedIn} alt="linkedin image" />
         </a>
-        <a href="" className="w-[1.5rem]" alt="twitter">
+        <a href="https://twitter.com/angelfunds5" className="w-[1.5rem]" alt="twitter">
             <Image src={Twitter} alt="twitter image" />
         </a>
-        <a href="" className="w-[1.2rem]" alt="wellfound">
+        <a href="https://angel.co/company/fundinit" className="w-[1.2rem]" alt="wellfound">
             <Image src={Wellfound} alt="wellfound image" />
         </a>
-        <a href="" className="w-[1.5rem]" alt="email">
+        <a href="mailto:pitch@fundinit.com" className="w-[1.5rem]" alt="email">
             <Image src={Email} alt="email image" />
         </a>
     </div>
--- components/Navbar.js
+++ components/Navbar.js
@@ -4,7 +4,9 @@ const Navbar = () => {
   return (
     <div className="flex justify-end pt-8 pr-[10rem] text-white bg-transparent gap-8">
         <Link href="#" alt="Home" legacyBehavior><a className="hover:text-purple-700">Home</a></Link>
-        <Link href="#" alt="Companies" legacyBehavior><a className="hover:text-purple-700">Companies</a></Link>
+        <a href="https://venture.angellist.com/fundinit/syndicate" alt="Companies" 
+        className="hover:text-purple-700">Companies
+        </a>
         <Link href="#" alt="Fellowship" legacyBehavior><a className="hover:text-purple-700">Fellowship</a></Link>
         <Link href="#" alt="Team" legacyBehavior><a className="hover:text-purple-700">Team</a></Link>
     </div>
