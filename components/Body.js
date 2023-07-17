47930c7bf4c6955cb9152562c5c20d42ff3b4df0
ahmad abdulazeez <ahmadabdulazeez354@gmail.com>
2023-07-17 09:33:55 +0100

    fix: mobile view

--- components/Body.js
+++ components/Body.js
@@ -2,9 +2,9 @@
 
 const Body = () => {
   return (
-    <div className="flex flex-col gap-6 text-white w-[40rem] mt-[7.5rem] ml-[11rem] items-start mb-[12.3rem]">
+    <div className="flex flex-col gap-6 text-white lg:w-[40rem] mt-[7.5rem] lg:ml-[11rem] lg:items-start mb-[12.3rem] items-center">
         <h1 className="text-[1.8rem] font-extrabold">Fundinit</h1>
-        <p className="text-[1.2rem]">An early-stage venture capital firm supporting the next generation
+        <p className="text-[1.2rem] text-center lg:text-left">An early-stage venture capital firm supporting the next generation
         Working on Healthcare, AI and solving real life problems.</p>
         <a href="https://4c3sfzuo0eo.typeform.com/to/EixmIok4" 
         className="px-6 py-2 border border-white hover:bg-purple-700">Pitch</a>
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,14 +1,47 @@
+import { useState } from "react"
+import Image from "next/image"
 import Link from "next/link"
+import MobileMenu from '@/public/images/menu.svg'
+import CancelMenu from '@/public/images/x.svg'
+
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleMenuClick = () => {
+    setIsOpen(!isOpen)
+  };
+  
+  
   return (
-    <div className="flex justify-end pt-8 pr-[10rem] text-white bg-transparent gap-8">
+    <div className="">
+      <div className="lg:hidden">
+        <div className="flex items-center justify-end">
+          <div className={`${isOpen? 'hidden':'block'}`} onClick={handleMenuClick}>
+            <Image src={MobileMenu} alt="hamburger" />
+          </div>
+          <div className={`${isOpen? 'block':'hidden'}`} onClick={handleMenuClick}>
+            <Image src={CancelMenu} alt="hamburger" />
+          </div>
+        </div>
+        
+        <div className={`flex flex-col items-center gap-8 pt-8 text-white bg-transparent ${isOpen? 'block':'hidden'}`}>
         <Link href="#" alt="Home" legacyBehavior><a className="hover:text-purple-700">Home</a></Link>
         <a href="https://venture.angellist.com/fundinit/syndicate" alt="Companies" 
         className="hover:text-purple-700">Companies
         </a>
         <Link href="#" alt="Fellowship" legacyBehavior><a className="hover:text-purple-700">Fellowship</a></Link>
         <Link href="#" alt="Team" legacyBehavior><a className="hover:text-purple-700">Team</a></Link>
+        </div>
+      </div>
+      
+      <div className="lg:flex justify-end pt-8 pr-[10rem] text-white bg-transparent gap-8 hidden ">
+        <Link href="#" alt="Home" legacyBehavior><a className="hover:text-purple-700">Home</a></Link>
+        <a href="https://venture.angellist.com/fundinit/syndicate" alt="Companies" 
+        className="hover:text-purple-700">Companies
+        </a>
+        <Link href="#" alt="Fellowship" legacyBehavior><a className="hover:text-purple-700">Fellowship</a></Link>
+        <Link href="#" alt="Team" legacyBehavior><a className="hover:text-purple-700">Team</a></Link>
+      </div>
     </div>
   )
 }
