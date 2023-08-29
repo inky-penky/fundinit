32cf155fc41a686411d64eda5a81f35df79c9827
ahmad abdulazeez <ahmadabdulazeez354@gmail.com>
2023-08-29 15:10:49 +0100

    feat: Body upgraded

--- components/Body.js
+++ components/Body.js
@@ -2,7 +2,7 @@
 
 const Body = () => {
   return (
-    <div className="flex flex-col gap-6 text-white lg:w-[40rem] mt-[7.5rem] lg:ml-[11rem] lg:items-start mb-[12.3rem] px-[2rem]">
+    <div className="flex flex-col gap-6 text-white lg:w-[40rem] mt-[7.5rem] lg:ml-[1.5rem] mb-[12.3rem] px-[2rem]">
         <h1 className="text-[1.8rem] font-extrabold w-[15rem]">Venture Capital for 
         Next generation </h1>
         <p className="text-[1rem] text-[#DAD1F180] max-w-[19em]">An early-stage venture capital firm supporting the next generation
