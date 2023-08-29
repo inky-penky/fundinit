64be7d5db071be858c8e6dd01a8f203181870359
ahmad abdulazeez <ahmadabdulazeez354@gmail.com>
2023-08-29 11:34:57 +0100

    feat: Navbar upgraded

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,46 +1,23 @@
-import { useState } from "react"
 import Image from "next/image"
-import Link from "next/link"
-import MobileMenu from '@/public/images/menu.svg'
-import CancelMenu from '@/public/images/x.svg'
+import Logo from "../public/images/fundinit_Logo.png"
+import Arrow from "../public/images/arrow_.svg"
+
 
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const handleMenuClick = () => {
-    setIsOpen(!isOpen)
-  };
+  
   
   
   return (
-    <div className="">
-      <div className="lg:hidden">
-        <div className="flex items-center justify-end pt-2 pr-2">
-          <div className={`${isOpen? 'hidden':'block'}`} onClick={handleMenuClick}>
-            <Image src={MobileMenu} alt="hamburger" />
-          </div>
-          <div className={`${isOpen? 'block':'hidden'}`} onClick={handleMenuClick}>
-            <Image src={CancelMenu} alt="hamburger" />
-          </div>
-        </div>
-        
-        <div className={`flex flex-col items-center gap-8 pt-8 text-white bg-transparent ${isOpen? 'block':'hidden'}`}>
-        <Link href="#" alt="Home" legacyBehavior><a className="hover:text-purple-700">Home</a></Link>
-        <a href="https://venture.angellist.com/fundinit/syndicate" alt="Companies" 
-        className="hover:text-purple-700">Companies
-        </a>
-        <Link href="#" alt="Fellowship" legacyBehavior><a className="hover:text-purple-700">Fellowship</a></Link>
-        <Link href="#" alt="Team" legacyBehavior><a className="hover:text-purple-700">Team</a></Link>
-        </div>
+    <div className="p-[1rem] lg:px-[5rem] lg:py-[2.5rem] flex items-center justify-between">
+      <div className="w-[5rem] lg:w-[10rem]">
+        <Image src={Logo} alt="logo" />
       </div>
-      
-      <div className="lg:flex justify-end pt-8 pr-[10rem] text-white bg-transparent gap-8 hidden ">
-        <Link href="#" alt="Home" legacyBehavior><a className="hover:text-purple-700">Home</a></Link>
-        <a href="https://venture.angellist.com/fundinit/syndicate" alt="Companies" 
-        className="hover:text-purple-700">Companies
-        </a>
-        <Link href="#" alt="Fellowship" legacyBehavior><a className="hover:text-purple-700">Fellowship</a></Link>
-        <Link href="#" alt="Team" legacyBehavior><a className="hover:text-purple-700">Team</a></Link>
+      <div className="flex items-center px-2 gap-3 py-1 lg:px-3 lg:gap-5 text-white rounded-full bg-[#CFCFCF36] ">
+        <a href="#" alt="" target="_blank" className="lg:text-[1.2rem]">contact</a>
+        <div className="w-[1.5rem]">
+          <Image src={Arrow} alt="arrow" />
+        </div>
       </div>
     </div>
   )
