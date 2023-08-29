1c1f193e38da56accee32ecc7ff149f827df3b9e
ahmad abdulazeez <ahmadabdulazeez354@gmail.com>
2023-08-29 15:08:25 +0100

    feat: Body upgraded

--- components/Body.js
+++ components/Body.js
@@ -2,12 +2,11 @@
 
 const Body = () => {
   return (
-    <div className="flex flex-col gap-6 text-white lg:w-[40rem] mt-[7.5rem] lg:ml-[11rem] lg:items-start mb-[12.3rem] items-center">
-        <h1 className="text-[1.8rem] font-extrabold">Fundinit</h1>
-        <p className="text-[1.2rem] text-center lg:text-left">An early-stage venture capital firm supporting the next generation
+    <div className="flex flex-col gap-6 text-white lg:w-[40rem] mt-[7.5rem] lg:ml-[11rem] lg:items-start mb-[12.3rem] px-[2rem]">
+        <h1 className="text-[1.8rem] font-extrabold w-[15rem]">Venture Capital for 
+        Next generation </h1>
+        <p className="text-[1rem] text-[#DAD1F180] max-w-[19em]">An early-stage venture capital firm supporting the next generation
         Working on Healthcare, AI and solving real life problems.</p>
-        <a href="https://4c3sfzuo0eo.typeform.com/to/EixmIok4" 
-        className="px-6 py-2 border border-white hover:bg-purple-700">Pitch</a>
     </div>
   )
 }
